3d8242d367d3fee313ddda0e6aa22d82f0195549
Sasha Rodriguez Carlomagno <caballerosasha@hotmail.com>
2022-04-18 13:54:22 -0300

    admin edited

--- avcapi/admin.py
+++ avcapi/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from models import Songs
+
+class SongsAdmin(admin.ModelAdmin):
+    fields    = ('id', 'nombre', 'año_de_composición', 'duración_segundos', 'compositores', 'letra')
+
+    #list of fields to display in django admin
+    list_display = ['id', 'nombre', 'año_de_composición', 'duración_segundos', 'compositores', 'letra']
+
+    #if you want django admin to show the search bar, just add this line
+    search_fields = ['nombre']
+
+    #to define model data list ordering
+    ordering = ('id','nombre')
+
+admin.site.register(Songs, SongsAdmin)
